f0476c647c5b67a57427953d246725e09cdcc32f
Vishal <vishalpal1501@gmail.com>
2024-03-18 08:45:31 +0530

    Start new project from here .Online Grocery Web Appliction

--- 3Diphone/src/components/ModelView.jsx
+++ 3Diphone/src/components/ModelView.jsx
@@ -1,5 +1,21 @@
-function ModelView() {
-  return <div>ModelView</div>;
+import { View } from '@react-three/drei';
+
+function ModelView({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className="border-2 border-red-500"
+    ></View>
+  );
 }
 
 export default ModelView;
