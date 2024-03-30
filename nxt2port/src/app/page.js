a33cbf71c614a777f5b3a845cabf50c05e1c5641
Vishal <vishalpal1501@gmail.com>
2024-03-30 18:50:57 +0530

    about card done

--- nxt2port/src/app/page.js
+++ nxt2port/src/app/page.js
@@ -5,8 +5,8 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="bg-green-800 h-[100vh]">
-      <div className="flex  md:flex-row flex-col justify-between w-full gap-2 md:p-20 p-4 bg-gradient-to-r from-blue-500 to-pink-500 ">
-        <div className="md:w-[20%]  p-2 md:h-[500px] bg-white md:sticky  relative left-0 md:top-32 top-32 border-2 border-red-900 rounded-xl">
+      <div className="flex  md:flex-row flex-col justify-between w-full gap-2 md:p-20 p-4 bg-gradient-to-l from-blue-500 to-pink-500 ">
+        <div className="md:w-[23%]  p-2 md:h-[500px] bg-white md:sticky  relative left-0 md:top-32 top-32 border-2 border-red-900 rounded-xl shadow-2xl">
           <Sabout />
         </div>
         <div className="md:w-[65%]  bg-white  md:order-none order-last border-2 border-red-900 rounded-xl p-9 relative md:top-12  top-32">
---
+++ nxt2port/src/utils/aboutData.js
@@ -0,0 +1,33 @@
+import {
+  Twitter,
+  Instagram,
+  LinkedIn,
+  EmailOutlined,
+  CalendarMonthOutlined,
+  PhoneAndroid,
+  LocationOnOutlined,
+} from '@mui/icons-material';
+import { pink } from '@mui/material/colors';
+import { color } from 'framer-motion';
+export const a1 = [
+  {
+    icon: <PhoneAndroid />,
+    name: 'Phone',
+    value: '+91 9598019848',
+  },
+  {
+    icon: <EmailOutlined />,
+    name: 'Email',
+    value: 'vishalpal1501@gmail.com',
+  },
+  {
+    icon: <LocationOnOutlined />,
+    name: 'Location',
+    value: 'Jaunpur UP',
+  },
+  {
+    icon: <CalendarMonthOutlined />,
+    name: 'DOB',
+    value: 'Jan 15, 20001',
+  },
+];
