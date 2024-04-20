23a73359c29eb65f65f9e0bff946b3ae825a2eef
Vishal <vishalpal1501@gmail.com>
2024-04-20 15:40:21 +0530

    3 projects done

--- nxt2port/src/utils/projectData.js
+++ nxt2port/src/utils/projectData.js
@@ -6,9 +6,18 @@ export const projectData = [
       'Woom is a video conferencing web application. Where admin create the meeting participants participate in the meeting room.For the user authentication we are use the Clerk library.Clerk is a complete suite of embeddable UIs, Flexible APIs, and  admin dashboards to authenticate and manage your user.Stream powers Chat messaging, video and audio activity feeds for billions of global end-users across thousands of different app.',
     image:
       'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/home.png',
-    technology: 'JavaScript, Next.js, Tailwind CSS, Clerk, Stream-io',
+    technology: [
+      'JavaScript',
+      'TypeScript',
+      'Next.js',
+      'Tailwind',
+      'CSS',
+      ' Clerk',
+      'Stream-io',
+    ],
     github: 'https://github.com/Plvishal/Zoom_nextjs',
-    liveLink: '',
+    liveLink:
+      'https://zoom-nextjs-phi.vercel.app/sign-in?redirect_url=https%3A%2F%2Fzoom-nextjs-phi.vercel.app%2F',
     gridImage: [
       'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/home.png',
       'https://res.cloudinary.com/de8rsflha/image/upload/v1713265093/PortfolioProjects/woom/upcoming.png',
@@ -24,48 +33,44 @@ export const projectData = [
   },
   {
     id: '2',
-    title: 'Zoom',
+    title: 'Airbnb',
     description:
-      'Woom is a video conferencing web application. Where admin create the meeting participants participate in the meeting room.For the user authentication we are use the Clerk library.Clerk is a complete suite of embeddable UIs, Flexible APIs, and  admin dashboards to authenticate and manage your user.Stream powers Chat messaging, video and audio activity feeds for billions of global end-users across thousands of different app.',
+      '• Hosts can list thier homes, apartments, rooms on Airbnb. They provide details about the space, amemties, pricing etc. First Host Create account on the Airbnb then can Add listing, Delete listing, Update listing• In our project Airbnb user only see the all listing if he logged in then he create the reviews for host listing.',
     image:
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/home.png',
-    technology: 'JavaScript, Next.js, Tailwind CSS, Clerk, Stream-io',
-    github: 'https://github.com/Plvishal/Zoom_nextjs',
-    liveLink: '',
-    gridImage: [
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/home.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265093/PortfolioProjects/woom/upcoming.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265093/PortfolioProjects/woom/signup.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/joinvialink.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/instantMeeting.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/signin.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/createMeeting.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/personalRoom.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265091/PortfolioProjects/woom/previous.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265091/PortfolioProjects/woom/recording.png',
+      'https://res.cloudinary.com/de8rsflha/image/upload/v1713345076/PortfolioProjects/airbnb/home.png',
+    technology: [
+      'JavaScript',
+      'Node.js',
+      'Express.js',
+      'EJS',
+      'CSS',
+      'HTML',
+      'MongoDB',
+      'Cloudinary',
     ],
+    github: 'https://github.com/Plvishal/Airbnb',
+    liveLink: 'https://exploreyourself.onrender.com/listings',
   },
   {
     id: '3',
-    title: 'Toom',
+    title: 'Ecommerce RESTFull APIs',
     description:
-      'Woom is a video conferencing web application. Where admin create the meeting participants participate in the meeting room.For the user authentication we are use the Clerk library.Clerk is a complete suite of embeddable UIs, Flexible APIs, and  admin dashboards to authenticate and manage your user.Stream powers Chat messaging, video and audio activity feeds for billions of global end-users across thousands of different app.',
-    image:
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/home.png',
-    technology: 'JavaScript, Next.js, Tailwind CSS, Clerk, Stream-io',
-    github: 'https://github.com/Plvishal/Zoom_nextjs',
-    liveLink: '',
-    gridImage: [
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/home.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265093/PortfolioProjects/woom/upcoming.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265093/PortfolioProjects/woom/signup.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/joinvialink.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/instantMeeting.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/signin.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/createMeeting.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265092/PortfolioProjects/woom/personalRoom.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265091/PortfolioProjects/woom/previous.png',
-      'https://res.cloudinary.com/de8rsflha/image/upload/v1713265091/PortfolioProjects/woom/recording.png',
+      'Using the power of JavaScript, Node.js, and Express.js, along with MongoDB as the database solution and Mongoose as the ODM (Object Data Modeling) library, this API provides a flexible and scalable architecture for managing products,orders, users, and authentication. bcryptjs is employed for secure password hashing and user authentication. Users can register, login, and manage their accounts securely. The API facilitates CRUD operations (Create, Read, Update, Delete) for managing products. Product data such as name, description, price, and inventory levels can be stored and retrieved from the database . nodemailer is utilized for sending transactional emails such as order confirmations, shipping notifications, and password reset instructions.',
+    technology: [
+      'JavaScript',
+      'Node.js',
+      'Express.js',
+      'MongoDB',
+      'bcryptjs',
+      'nodemailer',
+      'Mongoose',
+      'JWT Authentication',
     ],
+    image:
+      'https://res.cloudinary.com/de8rsflha/image/upload/v1713607115/PortfolioProjects/ecommerce_api/home.png',
+
+    github: 'https://github.com/Plvishal/Ecommerce-RestFull-API',
+    liveLink:
+      'https://www.postman.com/payload-geologist-38508759/workspace/storefeet-api/collection/31396654-ea59da66-d8b0-4b5c-b7e9-ef0b5118b4df?action=share&creator=31396654',
   },
 ];
